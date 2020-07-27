ab10e30fdc89a7c848d7aeb97aa81e22d1a3c153
rashi432 <66821017+rashi432@users.noreply.github.com>
2020-07-27 20:12:00 +0530

    initial

--- noteproject/src/app/app-routing.module.ts
+++ noteproject/src/app/app-routing.module.ts
@@ -1,11 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { TasksComponent } from './tasks/tasks.component';
+import { MainPageComponent } from './main-page/main-page.component';
 
 
 const routes: Routes = [
-  {path:'tasks', component:TasksComponent}
- 
 ];
 
 @NgModule({
